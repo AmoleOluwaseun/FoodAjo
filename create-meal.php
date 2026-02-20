2140978b7826e7780daa23536215138155d66a47
Amole Oluwaseun <oluwaseunamole002@gmail.com>
2026-02-20 15:59:22 +0100

    working on nav

--- create-meal.php
+++ create-meal.php
@@ -276,14 +276,28 @@
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="browse-meals.php">Browse Meals</a></li>
-                    <li><a href="dashboard.php">Dashboard</a></li>
-                    <li><a href="my-meals.php">My Meals</a></li>
-                    <li><a href="messages.php">Messages</a></li>
+                    <li><a href="how-it-works.php">How It Works</a></li>
+                    <li class="dropdown"><a href="#" class="active"><span>Meals</span> <i
+                                class="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                            <li><a href="create-meal.php">Create a Meal</a></li>
+                            <li><a href="my-meals.php">My Meals</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#"><span>Account</span> <i
+                                class="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                            <li><a href="dashboard.php">Dashboard</a></li>
+                            <li><a href="profile.php">My Profile</a></li>
+                            <li><a href="edit-profile.php">Edit Profile</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php#contact">Contact</a></li>
                 </ul>
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
             </nav>
 
-            <a class="btn-getstarted" href="create-meal.php">Create Meal</a>
+            <a class="btn-getstarted" href="login.php">Login</a>
         </div>
     </header>
 
