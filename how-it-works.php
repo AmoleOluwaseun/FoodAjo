2ad1ce84736d048435ff91b13e988b2ac7a4c3a6
Amole Oluwaseun <oluwaseunamole002@gmail.com>
2026-03-10 16:38:56 +0100

    working on browse loader

--- how-it-works.php
+++ how-it-works.php
@@ -706,6 +706,9 @@
     <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
     <!-- Vendor JS Files -->
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="assets/vendor/aos/aos.js"></script>
@@ -716,6 +719,9 @@
             once: true
         });
     </script>
+
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
 
 </body>
 
